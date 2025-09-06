58232bfe27db672c6aae407312ac9fdf959b0067
Saiful islam <saiful7745islam@gmail.com>
2025-09-06 14:45:24 +0600

    first commit

---
+++ script/index.js
@@ -0,0 +1,64 @@
+const loadLessons=()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res=>res.json())
+    .then((json)=> displayLesson(json.data));
+};
+
+  const loadLevelWord = (id)=>{
+    
+    const url =`https://openapi.programming-hero.com/api/level/${id}`;
+     fetch(url)
+     .then((res)=> res.json())
+     .then((data) => displayWord (data.data));
+
+  }
+
+  const displayWord = (words) => {
+       const wordContainer = document.getElementById ("word-container");
+      wordContainer.innerHTML = "";
+        if(words.length ===0){
+                 wordContainer.innerHTML = ` 
+                         <div class=" font-bangla text-center col-span-full rounded-xl py-10 space-y-6 ">
+                            <img class="mx-auto" src="assets/alert-error.png" />
+                         <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class="font-bold text-4xl">নেক্সট Lesson এ যান</h2>
+      </div>
+                 `;
+            return;
+        }
+        words.forEach (word =>{
+           // console.log(word);
+           const card = document.createElement("div");
+           card.innerHTML = ` 
+               <div class="bg-white rounded-xl shadow-sm text-center space-y-4 py-10 px-5">
+        <h2 class=" font-bold text-2xl">${word.word ?word.word: "শব্দ পাওয়া যায়নি"}</h2>
+        <p class="font-semibold">Meaning /Pronounciation</p>
+        <div class="font-bangla text-2xl font-medium">"${word.meaning ? word.meaning: "অর্থ পাওয়া যায়নি"}/ ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
+        <div class="flex justify-between items-center ">
+           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"> <i class="fa-solid fa-circle-info"></i></button>
+            <button class="btn  bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        </div>
+       </div>
+           `;
+           wordContainer.append(card);
+            
+        });
+  };
+
+displayLesson =(lessons) => {
+   // console.log(lessons);
+     const levelContainer =document.getElementById("level-container");
+       levelContainer.innerHTML = "";
+     for(let lesson of  lessons){
+        const btnDiv = document.createElement("div");
+          btnDiv.innerHTML = `
+    
+        <button onclick="loadLevelWord(${lesson.level_no})" class=" btn btn-outline btn-primary" > <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+        </button>
+         
+          `;
+        levelContainer.append(btnDiv);
+     }
+};
+
+loadLessons();
